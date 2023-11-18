b690b96976298c49f35d6ec37cac138ac6a1a9bd
Adrián <oadricabrera@gmail.com>
2023-11-18 20:25:42 -0300

    diccionario a DB

--- Crea estrategias y robots de trading algoritmico.py
+++ Crea estrategias y robots de trading algoritmico.py
@@ -16,12 +16,14 @@ precios_db=dict()
 for ticker in asset:
     precio_de_cierre[ticker]=yf.download(ticker,period="2y", interval="1d")["Adj Close"]
     precios_db[ticker]=yf.download(ticker,period="2y", interval="1d")["Open"]
+
 #print(precio_de_cierre)
 
-precio_de_cierre["EURUSD=x"].plot(figsize=(15,6))
-plt.show()
+#precio_de_cierre["EURUSD=x"].plot(figsize=(15,6))
+#plt.show()
 
-#print(precios_db)
+print(precios_db)
+
 
 
 
