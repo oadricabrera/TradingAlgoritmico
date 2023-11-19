3b4d8ccb05e637b681896654f6d4a0aab53dbb45
Isaias164 <sosaisaias250@gmail.com>
2023-11-18 21:58:54 -0300

    dataframe en un diccionario

--- Crea estrategias y robots de trading algoritmico.py
+++ Crea estrategias y robots de trading algoritmico.py
@@ -2,28 +2,33 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#df = yf.download("GOOG", interval="1d", start="2023-11-01", end=None)
+# df = yf.download("GOOG", interval="1d", start="2023-11-01", end=None)
 
-#df=yf.download("GOOG", period="10y", interval="1d")
+# df=yf.download("GOOG", period="10y", interval="1d")
 
-#varios tickers
+# varios tickers
 
-asset=["EURUSD=x","EURGBP=x","AUDUSD=x"]
+asset = ["EURUSD=x", "EURGBP=x", "AUDUSD=x"]
 
-precio_de_cierre=pd.DataFrame()
-precios_db=dict()
+precio_de_cierre = {}
 
 for ticker in asset:
-    precio_de_cierre[ticker]=yf.download(ticker,period="2y", interval="1d")["Adj Close"]
-    precios_db[ticker]=yf.download(ticker,period="2y", interval="1d")["Open"]
+    # precio_de_cierre[ticker] = yf.download(ticker, period="2y", interval="1d")[
+    #     "Adj Close"
+    # ]
+    precio_de_cierre.update({ticker: yf.download(ticker, period="2y", interval="1d")})
 
-#print(precio_de_cierre)
+    # precios_db[ticker] = yf.download(ticker, period="2y", interval="1d")
 
-#precio_de_cierre["EURUSD=x"].plot(figsize=(15,6))
-#plt.show()
+# print(precio_de_cierre)
 
-print(precios_db)
+# precio_de_cierre["EURUSD=x"].plot(figsize=(15,6))
+# plt.show()
 
-
-
-
+# cada ticker
+# print(precio_de_cierre.get("EURUSD=x").head())
+for fecha, precio in zip(
+    precio_de_cierre.get("EURUSD=x").index, precio_de_cierre.get("EURUSD=x")["Open"]
+):
+    # print(f"fecha {fecha} precio {precio}")
+    print({"fecha": fecha, "precio": precio})
