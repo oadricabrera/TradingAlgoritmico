aab2d28b82688127372b9482a91749b11edc18d2
Isaias164 <sosaisaias250@gmail.com>
2023-12-16 21:30:39 -0300

    refactoring métodos db

--- conexion_robot_udemy.py
+++ conexion_robot_udemy.py
@@ -1,4 +1,6 @@
 import pymysql
+from querys_sql import INSERT
+
 
 class RobotUdemy:
     HOST = "localhost"
@@ -36,7 +38,45 @@ class RobotUdemy:
         finally:
             return data
 
-    def grabar_data(self, simulacro=True, query=None, params=None, orden_campos=None):
+    # def grabar_data(self, simulacro=True, query=None, params=None, orden_campos=None):
+    #     """
+    #     [+] Esta método actualiza,elimina o inserta un registro en la bd
+
+    #     @query=None:str -> consulta sql que se va a ejecutar
+    #     @params=None:tuple | list -> valores de los parametros
+
+    #     @return:
+    #     int -> registros afectados
+    #     """
+    #     datos=None
+    #     if orden_campos:
+    #         datos = [params.get(campo) for campo in orden_campos]
+
+    #     if simulacro:
+    #         return datos
+    #     try:
+    #         cant_registro = 0
+    #         with self.connexion.cursor() as cursor:
+    #             if params is not None:
+    #                 cant_registro = cursor.execute(query, datos)
+    #             else:
+    #                 cant_registro = cursor.execute(query)
+    #             self.connexion.commit()
+    #     except Exception as e:
+    #         print(f"Error {str(e)}")
+    #     finally:
+    #         return cant_registro
+
+    def insertar_record(self, query=None, data=None):
+        self.grabar_data(query, data)
+
+    def actualizar_record(self, query=None, data=None):
+        self.grabar_data(query, data)
+
+    def eliminar_record(self, query=None, data=None):
+        self.grabar_data(query, data)
+
+    def grabar_data(self, query=None, data=None):
         """
         [+] Esta método actualiza,elimina o inserta un registro en la bd
 
@@ -46,27 +86,25 @@ class RobotUdemy:
         @return:
         int -> registros afectados
         """
-        datos=None
-        if orden_campos:
-            datos = [params.get(campo) for campo in orden_campos]
-        
-        if simulacro:
-            return datos
+        cant_registro = 0
         try:
-            cant_registro = 0
             with self.connexion.cursor() as cursor:
-                if params is not None:
-                    cant_registro = cursor.execute(query, datos)
-                else:
-                    cant_registro = cursor.execute(query)
+                cant_registro = (
+                    cursor.execute(query, data) if data else cursor.execute(query)
+                )
                 self.connexion.commit()
         except Exception as e:
             print(f"Error {str(e)}")
         finally:
             return cant_registro
-        
+
+
 conexion_robot_udemy = RobotUdemy()
+cant_insert = 0
+for i in range(10):
+    cant_insert += conexion_robot_udemy.insertar_record(
+        query=INSERT,
+        data=("2023-11-20", 1.090702772140503),
+    )
 
-conexion_robot_udemy.grabar_data(query="insert into nombre_tabla(fecha_cotizacion,precio_venta) values(%s,%s)", params=('2023-11-20', 1.090702772140503),simulacro=False)       #"insert into nombre_tabla(fecha,precio_venta) values(%s,%s)"      
-
-
+print(f"Registros insertados {cant_insert}")
---
+++ querys_sql.py
@@ -0,0 +1,3 @@
+INSERT = """
+    insert into nombre_tabla(fecha_cotizacion,precio_venta) values(%s,%s)
+"""
